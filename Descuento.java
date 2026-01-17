0ddb33bf68a407c7f25f99c91d5936fcd77ca0e2
kevinyasitmagos <20253RD021@utez.edu.mx>
2026-01-16 18:43:40 -0600

    Arreglé errores y dejé el programa funcionando

--- Descuento.java
+++ Descuento.java
@@ -1,29 +1,29 @@
-import.java.util.Scanner;
+import java.util.Scanner;
 
 public class Descuento{
    public static void main (String [] args){
-    Scanner sc = new scanner(System.in);
+    Scanner sc = new Scanner (System.in);
     final double DESCUENTO = .90;
 
     double precio1, precio2, precio3, precio4, precio5, resultado, totalDesc;
 
-    System.out.printl("Producto 1");
+    System.out.println("Producto 1");
     precio1 = sc.nextDouble();
 
-    System.out.printl("Producto 2");
+    System.out.println("Producto 2");
     precio2 = sc.nextDouble();
 
-    System.out.printl("Producto 3");
+    System.out.println("Producto 3");
     precio3 = sc.nextDouble();
 
-    System.out.printl("Producto 4");
+    System.out.println("Producto 4");
     precio4 = sc.nextDouble();
 
-    System.out.printl("Producto 5");
+    System.out.println("Producto 5");
     precio5 = sc.nextDouble();
 
     resultado = precio1+precio2+precio3+precio4+precio5;
     totalDesc = resultado*DESCUENTO;
-    System.out.printl("El total con descuento es " + totalDesc);
+    System.out.println("El total con descuento es " + totalDesc);
    } 
 }
