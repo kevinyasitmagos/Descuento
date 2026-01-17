c3f8d0d6a9f39eff59b9cce943e727b28334cc6c
kevinyasitmagos <20253RD021@utez.edu.mx>
2026-01-16 18:57:26 -0600

    Dejé el programa funcionando

--- AreaCirculo.java
+++ AreaCirculo.java
@@ -4,7 +4,7 @@ public class AreaCirculo{
    public static void main (String [] args){
     Scanner sc = new Scanner (System.in);
     
-    final double PI=3.1416
+    final double PI=3.1416;
     double radio, area;
 
         System.out.println("Radio del circulo:");
